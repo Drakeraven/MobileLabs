537f531b4eef73904076c2ed1ed1160d1908ec2d
Stephanie D <stephanie_day8@msn.com>
2018-04-06 13:27:57 -0700

    Finished Dialogs

--- app/src/main/java/stephd27/css/uw/tacoma/edu/dialogslab/DialogsActivity.java
+++ app/src/main/java/stephd27/css/uw/tacoma/edu/dialogslab/DialogsActivity.java
@@ -1,7 +1,9 @@
 package stephd27.css.uw.tacoma.edu.dialogslab;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DialogsActivity extends AppCompatActivity {
 
@@ -10,4 +12,24 @@ public class DialogsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialogs);
     }
+
+    public void launch(View view) {
+
+        DialogFragment fragment = null;
+        if (view.getId() == R.id.btn_fire_missiles) {
+            fragment = new FireMissilesDialogFragment();
+        } else if (view.getId() == R.id.btn_launch_colors) {
+            fragment = new ListDialogFragment();
+        } else if (view.getId() == R.id.btn_launch_toppings) {
+            fragment = new MultiListDialogFragment();
+        }else if (view.getId() == R.id.btn_launch_custom_dialog) {
+            fragment = new CustomDialogFragment();
+        }
+
+        if (fragment != null)
+            fragment.show(getSupportFragmentManager(), "launch");
+    }
+
+
+
 }

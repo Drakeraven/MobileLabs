2e4998f5d7c8d389a75878faf6f1ef0a6454292b
Stephanie D <stephanie_day8@msn.com>
2018-04-06 11:42:26 -0700

    Starting Dialogs Lab

---
+++ app/src/main/java/stephd27/css/uw/tacoma/edu/dialogslab/DialogsActivity.java
@@ -0,0 +1,13 @@
+package stephd27.css.uw.tacoma.edu.dialogslab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DialogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogs);
+    }
+}
---
+++ app/src/test/java/stephd27/css/uw/tacoma/edu/dialogslab/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package stephd27.css.uw.tacoma.edu.dialogslab;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
